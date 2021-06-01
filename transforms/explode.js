260c4a0dd9c70a52bd7999277babaf4dc9d33b41
Javier Luraschi <jluraschi@gmail.com>
2021-06-01 12:32:06 -0700

    display data in explode and type-cast arrays

--- transforms/explode.js
+++ transforms/explode.js
@@ -4,14 +4,23 @@
       label: 'Column'
 **/
 
-var exploded = [];
+if (column) {
+  if (!Array.isArray(data[0][column])) {
+    data = data.map(e => {
+      e[column] = Array.from(e[column].toString());
+      return e;
+    })
+  }
+  
+  var exploded = [];
 
-data = data.forEach((row, index) => {
-  exploded.push(...row[column].map(e => {
-    var result = Object.assign(row);
-    result[column] = e
-    return result;
-  }));
-});
+  data = data.forEach((row, index) => {
+    exploded.push(...row[column].map(e => {
+      var result = Object.assign(row);
+      result[column] = e
+      return result;
+    }));
+  });
 
-data = exploded;
+  data = exploded;
+}
