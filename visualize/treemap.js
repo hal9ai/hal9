a84efbb9762c50ac6ad199874b652593c4d7c420
Nataliya Stepanova <gnykka@gmail.com>
2021-06-05 01:40:43 +0300

    Add treemap chart script

--- visualize/treemap.js
+++ visualize/treemap.js
@@ -1,35 +1,65 @@
 /**
-  output: [ html ]
+  output: [html]
   params:
     - name: label
       label: Label
     - name: size
       label: Size
-  deps: [ 'd3.v6.min.js', 'chart.js', 'grid-chart.js', 'xy-chart.js', 'treemap-chart.js', 'chart-utils.js' ]
+  deps: [
+    'chart-utils.js',
+    'https://cdn.jsdelivr.net/npm/d3@6',
+    'https://cdn.jsdelivr.net/npm/@observablehq/plot@0.1',
+  ]
 **/
 
-var groupBy = function(xs, key) {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
-};
-
-const grouped = groupBy(data, label);
- 
+const groupedData = groupBy(data, label);
 const chartData = {
   name: 'root',
-  children: [],
+  children: Object.keys(groupedData).map(group => ({
+    name: group,
+    children: groupedData[group].map(g => ({
+      name: g[label] || g[size],
+      value: g[size],
+    })),
+  })),
 };
 
-chartData.children = Object.keys(grouped).map(group => {
-  return {
-    name: group,
-    children: grouped[group].map(g => {
-      return { name: g[label] ? g[label] : g[size], value: g[size] };
-    })
-  }
-});
+const hierarchy = d3.hierarchy(chartData).sum(d => d.value);
 
-const chart = new TreemapChart(chartData);
-chart.appendTo(wrapper(html));
+d3.treemap()
+  .size([html.clientWidth, html.clientHeight])
+  .padding(2)
+  (hierarchy);
+
+const leaves = hierarchy.leaves();
+
+html.appendChild(Plot.plot({
+  marks: [
+    Plot.rect(leaves, {
+      x1: 'x0', x2: 'x1',
+      y1: 'y0', y2: 'y1',
+      fill: d => d.data.name,
+      title: d => `${d.data.name}: ${d.value?.toFixed(2)}`,
+    }),
+    Plot.text(leaves, {
+      x: 'x0', y: 'y1',
+      dx: 10, dy: 10,
+      text: d => {
+        const v = d.value?.toFixed(1);
+        const width = (v.length - 1) * 8 + 5;
+        const height = 15;
+
+        return (d.x1 - d.x0 > width && d.y1 - d.y0 > height) ? v : '';
+      },
+      fill: '#fff',
+    }),
+  ],
+  x: { axis: null },
+  y: { axis: null },
+  width: html.clientWidth,
+  height: html.clientHeight,
+  style: {
+    background: hal9.isDark() ? '#222' : '',
+    color: hal9.isDark() ? 'white' : ''
+  },
+}));
