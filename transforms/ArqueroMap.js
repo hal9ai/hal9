12538a62e9b2dfd3c7edd1cc7ce0d1ea088ccd68
Anchit Sadana <70300199+anchit-sadana@users.noreply.github.com>
2021-08-05 13:32:10 -0700

    Add ArqueroMap

---
+++ transforms/ArqueroMap.js
@@ -0,0 +1,16 @@
+/**
+  params:
+    - name: column
+      label: Column
+      value:
+        - control: 'textbox'
+    - name: expression
+      label: 'Expression'
+      value:
+        - control: 'textbox'
+          value: row.id = index
+**/
+
+if (expression && column) {
+  data = data.derive(column : data => expression
+}
