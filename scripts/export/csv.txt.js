0684d398eedcf4b8e2db016ba16700b065d87c44
Javier Luraschi <jluraschi@gmail.com>
2021-08-25 01:01:48 -0400

    add script to export data as csv

---
+++ scripts/export/csv.txt.js
@@ -0,0 +1,20 @@
+/**
+  output: [ 'data', 'html' ]
+  deps:
+    - https://cdn.jsdelivr.net/npm/hal9-utils@0.0.4/dist/hal9-utils.min.js
+    - 'https://cdnjs.cloudflare.com/ajax/libs/d3/6.2.0/d3.min.js'
+**/
+
+data = await hal9.utils.toRows(data);
+
+const lines = [ d3.csvFormat(data) ];
+var file = new Blob(lines, { type: 'text/plain' });
+
+var a = document.createElement('a');
+a.href = URL.createObjectURL(file);
+a.download = 'data.csv';
+a.innerHTML = 'Download data as CSV!'
+a.style.display = 'block';
+a.style.marginTop = '45%';
+
+html.appendChild(a);
