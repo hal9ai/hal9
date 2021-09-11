2fd2f20243a8cc5f296f57014946aa447c7d5cee
Javier Luraschi <jluraschi@gmail.com>
2021-09-10 23:44:48 -0700

    progress towards implementing api support

---
+++ src/core/pipelineremote.js
@@ -0,0 +1,25 @@
+import * as workers from './workers'
+import * as environment from './utils/environment'
+
+async runPipelineRemote(pipelineid) {
+  var workerUrl = await workers.getValidWorkerUrl();
+  const pipelineurl = environment.getServerUrl() + '/?sharedPipeline=' + pipelineid;
+
+  var res = await fetch(workerUrl + '/execute', {
+    method: 'POST',
+    body: JSON.stringify({ operation: 'pipeline', pipelineurl: pipelineurl }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (!res.ok) {
+    var details = res.statusText;
+    try {
+      details = await res.json();
+    }
+    catch (e) {}
+
+    details = typeof(details) === 'string' ? details : JSON.stringify(details);
+    throw 'Failed to execute pipeline on remote worker: ' + details;
+  }
+  return await res.json();
+}
