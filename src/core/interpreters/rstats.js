8250e234fd1cc36fe84f4288c839672a0591e5c4
Javier Luraschi <jluraschi@gmail.com>
2021-09-28 16:27:08 -0700

    add support to capture r plots

--- src/core/interpreters/rstats.js
+++ src/core/interpreters/rstats.js
@@ -4,6 +4,9 @@ export default function(script, header) {
   const params = header.params ? header.params.map(e => e.name) : [];
   const inputs = header.input ? header.input : [];
 
+  // escape script
+  script = script.replace(/`/g, '\\\`');
+
   const paramsAll = params;
   paramsAll.push(...inputs);
 
@@ -21,17 +24,18 @@ if (!fs.existsSync('./rscript/')) fs.mkdirSync('./rscript/');
 const scriptpath = path.resolve('./rscript/', Math.random().toString());
 if (!fs.existsSync(scriptpath)) fs.mkdirSync(scriptpath);
 
-var paramsserial = {};
-Object.keys(params).map(e => paramsserial[e] = params[e])
+params["hal9__scriptpath"] = scriptpath;
 
 const paramsname = path.resolve(scriptpath, 'params.json');
-await writeFileAsync(paramsname, JSON.stringify(paramsserial));
+await writeFileAsync(paramsname, JSON.stringify(params));
 
 const scriptname = path.resolve(scriptpath, 'code.R');
 const outputname = path.resolve(scriptpath, 'output.json');
 await writeFileAsync(scriptname, \`
 
 hal9__params = jsonlite::read_json('\${paramsname}')
+
+options(device = function() png(file.path(hal9__params$hal9__scriptpath, 'hal9__plot%03d.png')))
   
 ${paramRDef}
 
@@ -47,7 +51,22 @@ const { stdout, stderr } = await exec('Rscript ' + scriptname, { timeout: 30000 
 const rawoutput = await readFileAsync(outputname)
 const output = JSON.parse(rawoutput);
 
-data = output.data
+var fileslist = fs.readdirSync(scriptpath);
+
+function fileToMediaType(name) {
+  var ext = name.split('.').pop();
+  var extMediaType = {
+    'png': 'image/png',
+    'jpg': 'image/jpg',
+    'jpeg': 'image/jpeg',
+  };
+  return extMediaType[ext] ? extMediaType[ext] : 'text/plain';
+}
+
+data = output.data;
+var files = {};
+fileslist.map(e => files[e] = 'data:' + fileToMediaType(e) + ';base64,' + fs.readFileSync(path.resolve(scriptpath, e), { encoding: 'base64' }));
+
 fs.rmSync(scriptpath, { recursive: true });
   `;
 }
