94e694994ed44bef0eeb209809de99fe19d6541c
anchit sadana <anchitsadana@anchits-MacBook-Pro.local>
2021-11-14 16:50:41 -0800

    add rollingsum

---
+++ scripts/transforms/rollingaverage.txt.js
@@ -0,0 +1,14 @@
+/**
+  params:
+    - name: column
+      label: Column
+  deps:
+    - https://cdn.jsdelivr.net/npm/arquero@latest
+    - https://cdn.jsdelivr.net/npm/hal9-utils@latest/dist/hal9-utils.min.js 
+**/
+
+data = await hal9.utils.toArquero(data);
+if (column) {
+  data = data.params({column}).derive({ ra: aq.rolling((d, $) => aq.op.sum(d[$.column])) });
+ data = data.rename({ ra : 'rollingSum_'+column });
+}
