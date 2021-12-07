dd5267b23bda844b43455a4d5cff3639d060d88d
inaki321 <90475192+inaki321@users.noreply.github.com>
2021-12-07 14:49:46 -0600

    Update googlesheets.txt.js

--- scripts/services/googlesheets.txt.js
+++ scripts/services/googlesheets.txt.js
@@ -1,22 +1,20 @@
 /**
   input: []
   params:
-    - name: gid
-      label: 'Google sheets page gid'
+    - name: url
+      label: 'Google sheets page URL'
       value:
         - control: 'textbox'
-    - name: id
-      label: 'Google sheets id'
-      value:
-        - control: 'textbox'
-
   deps:
     - https://cdn.jsdelivr.net/npm/arquero@latest
     - https://cdn.jsdelivr.net/npm/hal9-utils@latest/dist/hal9-utils.min.js
 **/
 
 
+url = url.replace("https://docs.google.com/spreadsheets/d/","")
+url = url.split("/edit#gid=")
 
-var url = "https://docs.google.com/spreadsheets/d/e/"+id+"/pub?gid="+gid+"&single=true&output=csv"
+url = "https://docs.google.com/spreadsheets/d/"+url[0]+"/export?format=csv&gid="+url[1]
 
-var data= aq.fromCSV(await fetch(url).then(res => res.text()));
+var data= aq.fromCSV(await fetch(url).then(res => res.text())); 
+
