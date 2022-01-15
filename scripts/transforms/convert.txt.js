86bf8dd3af68fae4e46a03692ca6a09c5da59829
inaki321 <90475192+inaki321@users.noreply.github.com>
2022-01-14 18:42:20 -0600

    Convert block update

--- scripts/transforms/convert.txt.js
+++ scripts/transforms/convert.txt.js
@@ -3,19 +3,82 @@
     - name: field
       label: Field
       single: true
-    - name: expression
-      label: 'Expression'
+    - name: dataType
+      label: 'Data type'
+      value:
+        - control: 'select'
+          value: ''
+          values:
+            - name: int
+              label: Integer
+            - name: float
+              label: Float
+            - name: string
+              label: String
+            - name: date
+              label: Date
+            - name: bool
+              label: Boolean
+
+    - name: charactersToRemove
+      label: Remove Characters
       value:
         - control: 'textbox'
-          value: new Date(field)
+
+
   deps:
     - https://cdn.jsdelivr.net/npm/arquero@latest
     - https://cdn.jsdelivr.net/npm/hal9-utils@latest/dist/hal9-utils.min.js 
 **/
 
+
 data = await hal9.utils.toArquero(data);
-if (expression && field) {
-  var mapExp = new Function('field', 'return '+ expression);
-  data = data.params({field}).derive({ column: aq.escape((data, $) => mapExp(data[$.field])) });
-  data = data.rename({ column : field });
+let values;
+
+//int conversion
+if ((dataType === 'int') || (dataType === 'float') ){
+  values = data.array(field).map(value => {
+    let result = value;
+    if (typeof result === 'string') {
+      result = result.replaceAll(' ', '');
+      result = result.replaceAll(',', '');
+      result = result.replaceAll('$', '');
+      result = result.replaceAll(new RegExp(charactersToRemove, 'g'), '');
+
+      if (dataType === 'int') {
+        result = parseInt(result);
+      } else if (dataType === 'float') {
+        result = parseFloat(result);
+      }
+    }
+    return result;
+  });
 }
+//date conversion
+else if (dataType === 'date') {
+  values = data.array(field).map(value => {
+    return new Date(value);
+  });
+}
+//string conversion
+else if (dataType === 'string') {
+  values = data.array(field).map(value => {
+    return String(value);
+  });
+}
+//bool conversion
+else if (dataType === 'bool') {
+  values = data.array(field).map(value => {
+    if (typeof value === 'string') {
+      value = value.replaceAll(new RegExp(charactersToRemove, 'g'), '');
+      value = value.trim().toLowerCase();
+      const falseStrings = ['', '0', '0.0', '-0', '0n', 'false', 'null', 'undefined', 'nan'];
+      return !(falseStrings.includes(value));
+    } else {
+      return Boolean(value);
+    }
+  });
+}
+
+data = data.assign({ field: values });
+data = data.rename({ field: field });
