dd17980be61a448aeba061d77e6be925c1008ddd
anchit-sadana <asadana@berkeley.edu>
2022-01-28 14:17:53 -0800

    add boxplot

---
+++ scripts/charts/boxplot.txt.js
@@ -0,0 +1,24 @@
+##
+## deps: [ 'numpy', 'pandas', 'micropip' ]
+## output: ['plot']
+## params:
+##  - name: x
+##    label: x
+##  - name: y
+##    label: y
+##
+
+import numpy as np
+import pandas as pd
+import micropip
+await micropip.install('seaborn')
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if 'data' in dict(globals()):
+  data = pd.DataFrame(data)
+else:
+  data = pd.DataFrame({'Name':[ 'Tom', 'Nick' ], 'Age':[ 56, 33 ]})
+if (x and y):
+  sns.boxplot(x = data[x], y = data[y])
+  plt.show()
