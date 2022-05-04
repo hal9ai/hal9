8e8780842df9fed13482e04282b59521d5d54252
Javier Luraschi <jluraschi@gmail.com>
2022-05-04 14:28:56 -0700

    get r package to render data

--- r/inst/htmlwidgets/hal9.js
+++ r/inst/htmlwidgets/hal9.js
@@ -5,20 +5,16 @@ HTMLWidgets.widget({
     return {
 
       renderValue: function(x) {
-        const html = `
-<script>
-  window.hal9 = {
-    data: [ { "number": 1 }, { "number": 2 } ],
-    pipeline: {
-      "steps": [ { "name": "javascript", "label": "Source", "language": "javascript", "id": 1, } ],
-      "params": {}, "outputs": {}, "scripts": { "1": "data = window.hal9.data" },
-      "version": "0.0.1"
-    }
-  }
-</script>
-<div id="app" style="height: 420px; max-height: 420px;"></div>
-        `;
+        window.hal9 = {
+          data: [ { "number": 1 }, { "number": 2 } ],
+          pipeline: {
+            "steps": [ { "name": "javascript", "label": "Source", "language": "javascript", "id": 1, } ],
+            "params": {}, "outputs": {}, "scripts": { "1": "data = window.hal9.data" },
+            "version": "0.0.1"
+          }
+        };
 
+        const html = `<div id="app" style="height: 420px; max-height: 420px;"></div>`;
         el.innerHTML = html;
 
         const script = document.createElement('script');
