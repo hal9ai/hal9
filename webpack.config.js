ce4aa4688d0bb16d56e7c67b652d288be6580e3a
Greg <88065578+G-M-E@users.noreply.github.com>
2022-05-05 15:19:03 -0700

    Try @babel/runtime-corejs3

--- webpack.config.js
+++ webpack.config.js
@@ -37,16 +37,15 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: 'usage',
-                  corejs: '3.22'
-                }
-              ]
+              '@babel/preset-env'
             ],
             plugins: [
-              '@babel/plugin-transform-runtime'
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  corejs: 3
+                }
+              ]
             ]
           }
         }
