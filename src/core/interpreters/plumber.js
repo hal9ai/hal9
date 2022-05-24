118346855d71218f62a3dd65dc2f4ad204d022f9
Javier Luraschi <jluraschi@gmail.com>
2022-05-24 16:09:05 -0700

    add support to modify kill and modify api code after deployment

--- src/core/interpreters/plumber.js
+++ src/core/interpreters/plumber.js
@@ -43,6 +43,7 @@ if (!fs.existsSync(scriptpath)) fs.mkdirSync(scriptpath);
 const plumberscript = path.resolve(scriptpath, 'plumber.R');
 const paramsname = path.resolve(scriptpath, 'params.json');
 
+const apiscript = \`${script}\`;
 const finalscript = \`
   hal9__params = jsonlite::read_json('\${paramsname}', simplifyVector = TRUE)
   ${paramRDef}
@@ -54,30 +55,45 @@ function(msg="") {
   list(healthy = 'true')
 }
 
-${script}
+\${apiscript}
 \`;
 
 if (!fs.existsSync('./hal9__ports/')) fs.mkdirSync('./hal9__ports/');
-const scriptmd5 = crypto.createHash('md5').update(finalscript).digest("hex");
+const scriptmd5 = crypto.createHash('md5').update(apiscript).digest("hex");
 const portsfile = './hal9__ports/' + portnumber + '.json';
+
+const updatePortsFile = (hash, pid) => {
+  const rawports = JSON.stringify({
+    hash: hash,
+    pid: pid
+  });
+  fs.writeFileSync(portsfile, rawports);
+}
+
 let portsdata = {};
 if (fs.existsSync(portsfile)) {
   try {
-  const rawports = fs.readFileSync(portsfile);
-  portsdata = JSON.parse(rawports);
+    const rawports = fs.readFileSync(portsfile);
+    portsdata = JSON.parse(rawports);
   } catch(e) {
     console.log('Error reading ports file: ' + e.toString())
   }
 }
 else {
-  const rawports = JSON.stringify({
-    hash: scriptmd5
-  });
-  fs.writeFileSync(portsfile, rawports);
+  updatePortsFile(scriptmd5)
 }
 
 if (portsdata.hash && portsdata.hash != scriptmd5) {
-  console.log('The api has changed from ' + portsdata.hash + ' to ' + scriptmd5 + ', redeploying.')
+  console.log(\`The api with pid \${portsdata.pid} has changed from \${portsdata.hash} to \${scriptmd5} redeploying.\`)
+  if (portsdata.pid) {
+    try {
+      await exec('kill -9 ' + portsdata.pid)
+      console.log('Killed process ' + portsdata.pid)
+    }
+    catch(e) {
+      console.log('Failed to kill: ' + e.toString())
+    }
+  }
 }
 
 await writeFileAsync(plumberscript, finalscript);
@@ -104,12 +120,18 @@ const apilocalurl = 'http://localhost:' + portnumber;
 const isHealthy = async () => {
   try {
     const healthurl = apilocalurl + '/health';
-
     console.log('Checking health for ' + healthurl)
+
+    const controller = new AbortController()
+    const timeoutId = setTimeout(() => controller.abort(), 100)
+
     const healthresult = await fetch(healthurl, {
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' },
+      signal: controller.signal
     });
+
+    clearTimeout(timeoutId)
 
     return healthresult.ok;
   }
@@ -121,9 +143,12 @@ const isHealthy = async () => {
 
 var stderr = '';
 var output = {};
+hal9__error = undefined;
 
 var forker = (accept, reject) => {
-  const spawned = spawn('Rscript', [ scriptname ], { timeout: 5000 });
+  const spawned = spawn('Rscript', [ scriptname ], { timeout: 20000 });
+
+  updatePortsFile(scriptmd5, spawned.pid)
 
   spawned.stdout.on('data', (data) => {
     console.log(data.toString())
@@ -138,19 +163,21 @@ var forker = (accept, reject) => {
   });
 
   spawned.on('error', (err) => {
-    hal9__error = stderr;
-    hal9__error = hal9__error + 'Error: ' + err;
-    reject(hal9__error)
+    // hal9__error = stderr;
+    // hal9__error = hal9__error + 'Error: ' + err;
+    // if (reject) reject(hal9__error)
   });
 
   spawned.on('exit', (code, signal) => {
-    if (code != 0 || signal) {
+    if ((code != null && code > 0) || (signal && signal.toString() != 'SIGKILL')) {
+      console.log('Process exited with code [' + code + '] and signal [' + signal + ']');
+      
       hal9__error = stderr;
-      if (signal) hal9__error = hal9__error + 'Signal: ' + signal;
-      reject(hal9__error)
+      if (signal) hal9__error = hal9__error + 'Signal: [' + signal + ']';
+      if (reject) reject(hal9__error)
     }
     else {
-      accept()
+      if (accept) accept()
     }
   });
 }
@@ -159,14 +186,43 @@ let forked = undefined;
 
 if (!await isHealthy()) {
   console.log('Forking new API process')
-  forked = new Promise(forker);
-  // await forked;
+  forker()
 
-  await new Promise(r => setTimeout(r, 1000));
-}
+  var initTimeoutSec = 60;
+  const forkPromise = new Promise((accept, reject) => {
+    const waitInitialized = async () => {
+      initTimeoutSec = initTimeoutSec - 1;
 
-if (!await isHealthy()) {
-  throw 'Failed to retrieve data from: ' + apilocalurl + '/health';
+      try {
+        if (hal9__error) {
+          console.log('Error while initializing API: ' + hal9__error)
+          reject(hal9__error)
+        }
+        else if (initTimeoutSec <= 0) {
+          console.log('Failed to initialize after ' + initTimeoutSec + 'seconds')
+          reject('Failed to initialize after ' + initTimeoutSec + 'seconds')
+        }
+        else if (!await isHealthy()) {
+          console.log('API is not healthy, waiting')
+          setTimeout(waitInitialized, 1000);
+        }
+        else {
+          console.log('API is healthy, success!')
+          accept()
+        }
+      }
+      catch(e) {
+        console.log('API init failed: ' + e.toString())
+        reject(e)
+      }
+    };
+
+    waitInitialized();
+  })
+
+  await forkPromise;
+  
+  // await new Promise(r => setTimeout(r, 1000));
 }
 
 const apiparams = Object.assign(
@@ -205,7 +261,8 @@ fs.rmSync(scriptpath, { recursive: true });
 
 return {
   data: datares
-}`
+}
+  `;
 
   return {
     script: interpreted
