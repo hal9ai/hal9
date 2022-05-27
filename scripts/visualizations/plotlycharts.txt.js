802ed1cbee1f4726cd7083868cea6169f88945cd
anchit-sadana <asadana@berkeley.edu>
2022-05-27 12:04:32 -0700

    more descriptive error message for the plotly block

--- scripts/visualizations/plotlycharts.txt.js
+++ scripts/visualizations/plotlycharts.txt.js
@@ -23,10 +23,10 @@
               label: 2D Histogram
 
     - name: dataSizes
-      label: 'data sizes'
+      label: 'Marker Size'
       value: 
         - control: 'number'
-          value: 1
+          value: 5
     - name: palette
       label: Chart Palette
       value:
@@ -45,173 +45,184 @@
     - https://cdn.plot.ly/plotly-latest.min.js
     - https://cdn.jsdelivr.net/npm/arquero@latest
 **/
-
-data = await hal9.utils.toArquero(data);
-
-console.log('paleta: ' + palette)
-
-var layout = {
-    title: x + ' - ' + y + ' ' + chartType,
-    xaxis: {
-        title: x,
-        showgrid: false,
-        zeroline: false
-    },
-    yaxis: {
-        title: y,
-        showline: false
-    },
-    width: html.offsetWidth,
-
-
-}
-
-//colors
-/*            - schemeTableau10
-            - schemeAccent
-            - schemeDark2
-            - schemePaired
-            - schemeSet1
-            - schemeSet2
-            - schemeSet3
-*/
-let dataColor;
-switch (palette) {
-    case 'schemeTableau10':
-        dataColor = 'rgb(14,101,152)';
-        break;
-    case 'schemeAccent':
-        dataColor = 'rgb(93,202,114)';
-        break;
-    case 'schemeDark2':
-        dataColor = 'rgb(75,160,111)';
-        break;
-    case 'schemePaired':
-        dataColor = 'rgb(168,205,222)';
-        break;
-    case 'schemeSet1':
-        dataColor = 'rgb(230,35,35)';
-        break;
-    case 'schemeSet2':
-        dataColor = 'rgb(123,201,169)';
-        break;
-    case 'schemeSet3':
-        dataColor = 'rgb(123,201,194)';
-        break;
-}
-let x_data = data.array(x)
-let texta = [];
-for (i in x_data) {
-    texta.push(y);
-}
-let trace, chartData;
-switch (chartType) {
-    case 'lines':
-        {
-            linesTrace = {
-                x: data.array(x),
-                y: data.array(y),
-                mode: 'lines',
-                name: '',
-                hovertemplate:
-                    "x_" + x + ': %{x}<br>' +
-                    "y_" + y + ': %{y}<br>',
-                line: {
-                    color: dataColor,
-                    width: dataSizes
+if (data == null) {
+    throw 'No input data defined. Please use an import block to import data before visualizing it.'
+  }
+  data = await hal9.utils.toArquero(data);
+  
+  console.log('paleta: ' + palette)
+  if (x == null){
+    throw 'Please select a column for the x-axis'
+  }
+  if (y == null){
+    throw 'Please select a column for the y-axis'
+  }
+  if (chartType == null){
+    throw 'Please select a Chart Type'
+  }
+    var layout = {
+        title: x + ' - ' + y + ' ' + chartType,
+        xaxis: {
+            title: x,
+            showgrid: false,
+            zeroline: false
+        },
+        yaxis: {
+            title: y,
+            showline: false
+        },
+        width: html.offsetWidth,
+  
+  
+    }
+  
+    //colors
+    /*            - schemeTableau10
+                - schemeAccent
+                - schemeDark2
+                - schemePaired
+                - schemeSet1
+                - schemeSet2
+                - schemeSet3
+    */
+    let dataColor;
+    switch (palette) {
+        case 'schemeTableau10':
+            dataColor = 'rgb(14,101,152)';
+            break;
+        case 'schemeAccent':
+            dataColor = 'rgb(93,202,114)';
+            break;
+        case 'schemeDark2':
+            dataColor = 'rgb(75,160,111)';
+            break;
+        case 'schemePaired':
+            dataColor = 'rgb(168,205,222)';
+            break;
+        case 'schemeSet1':
+            dataColor = 'rgb(230,35,35)';
+            break;
+        case 'schemeSet2':
+            dataColor = 'rgb(123,201,169)';
+            break;
+        case 'schemeSet3':
+            dataColor = 'rgb(123,201,194)';
+            break;
+    }
+    let x_data = data.array(x)
+    let texta = [];
+    for (i in x_data) {
+        texta.push(y);
+    }
+    let trace, chartData;
+    switch (chartType) {
+        case 'lines':
+            {
+                linesTrace = {
+                    x: data.array(x),
+                    y: data.array(y),
+                    mode: 'lines',
+                    name: '',
+                    hovertemplate:
+                        "x_" + x + ': %{x}<br>' +
+                        "y_" + y + ': %{y}<br>',
+                    line: {
+                        color: dataColor,
+                        width: dataSizes
+                    }
+                };
+                chartData = [linesTrace];
+                break;
+            }
+  
+        case 'scatter':
+            {
+                scatterTrace = {
+                    x: data.array(x),
+                    y: data.array(y),
+                    mode: 'markers',
+                    type: 'scatter',
+                    name: '',
+                    hovertemplate:
+                        "x_" + x + ': %{x}<br>' +
+                        "y_" + y + ': %{y}<br>',
+                    marker: {
+                        size: dataSizes,
+                        color: dataColor
+                    },
+                };
+                chartData = [scatterTrace];
+                break;
+            }
+  
+        case 'barChart':
+            {
+                barTrace = {
+                    x: data.array(x),
+                    y: data.array(y),
+                    name: '',
+                    type: 'bar',
+                    hovertemplate:
+                        "x_" + x + ': %{x}<br>' +
+                        "y_" + y + ': %{y}<br>',
+                    marker: {
+                        size: dataSizes,
+                        color: dataColor
+                    },
                 }
-            };
-            chartData = [linesTrace];
-            break;
-        }
-
-    case 'scatter':
-        {
-            scatterTrace = {
-                x: data.array(x),
-                y: data.array(y),
-                mode: 'markers',
-                type: 'scatter',
-                name: '',
-                hovertemplate:
-                    "x_" + x + ': %{x}<br>' +
-                    "y_" + y + ': %{y}<br>',
-                marker: {
-                    size: dataSizes,
-                    color: dataColor
-                },
-            };
-            chartData = [scatterTrace];
-            break;
-        }
-
-    case 'barChart':
-        {
-            barTrace = {
-                x: data.array(x),
-                y: data.array(y),
-                name: '',
-                type: 'bar',
-                hovertemplate:
-                    "x_" + x + ': %{x}<br>' +
-                    "y_" + y + ': %{y}<br>',
-                marker: {
-                    size: dataSizes,
-                    color: dataColor
-                },
+                chartData = [barTrace];
+                break;
             }
-            chartData = [barTrace];
-            break;
-        }
-    case 'fillArea':
-        {
-            fillTrace = {
+        case 'fillArea':
+            {
+                fillTrace = {
+                    x: data.array(x),
+                    y: data.array(y),
+                    name: '',
+                    type: 'scatter',
+                    fill: 'tozeroy',
+                    hovertemplate:
+                        "x_" + x + ': %{x}<br>' +
+                        "y_" + y + ': %{y}<br>',
+                    marker: {
+                        size: dataSizes,
+                        color: dataColor
+                    },
+                }
+                chartData = [fillTrace];
+                break;
+            }
+        case 'histogram':
+            {
+                fillTrace = {
+                    x: data.array(x),
+                    name: '',
+                    type: 'histogram',
+                    hovertemplate:
+                        "x_" + x + ': %{x}<br>',
+                    marker: {
+                        size: dataSizes,
+                        color: dataColor
+                    },
+                }
+                chartData = [fillTrace];
+                break;
+            }
+        case 'twoHistogram': {
+            twohist = {
                 x: data.array(x),
                 y: data.array(y),
                 name: '',
-                type: 'scatter',
-                fill: 'tozeroy',
-                hovertemplate:
-                    "x_" + x + ': %{x}<br>' +
-                    "y_" + y + ': %{y}<br>',
-                marker: {
-                    size: dataSizes,
-                    color: dataColor
-                },
-            }
-            chartData = [fillTrace];
-            break;
-        }
-    case 'histogram':
-        {
-            fillTrace = {
-                x: data.array(x),
-                name: '',
-                type: 'histogram',
+                type: 'histogram2d',
                 hovertemplate:
                     "x_" + x + ': %{x}<br>',
-                marker: {
-                    size: dataSizes,
-                    color: dataColor
-                },
+  
             }
-            chartData = [fillTrace];
+            chartData = [twohist];
             break;
         }
-    case 'twoHistogram': {
-        twohist = {
-            x: data.array(x),
-            y: data.array(y),
-            name: '',
-            type: 'histogram2d',
-            hovertemplate:
-                "x_" + x + ': %{x}<br>',
-
-        }
-        chartData = [twohist];
-        break;
+  
     }
-
-}
-
-Plotly.newPlot(html, chartData, layout);
+  
+    Plotly.newPlot(html, chartData, layout);
+  
