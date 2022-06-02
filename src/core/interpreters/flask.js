3f473e6fc4b3f647414adaa7c234b6c340268da4
Javier Luraschi <jluraschi@gmail.com>
2022-06-02 09:26:24 -0700

    add support for multiple plumber apis in same worker

--- src/core/interpreters/flask.js
+++ src/core/interpreters/flask.js
@@ -1,9 +1,9 @@
 import { debuggerIf } from '../utils/debug'
+import { portScripts } from './apicore'
 
 const reservedOutput = [];
 
 export default async function(script, header, context, step) {
-  // context.pipelinepath
   const debugcode = debuggerIf('interpret');
 
   const params = header.params ? header.params.map(e => e.name) : [];
@@ -30,52 +30,12 @@ export default async function(script, header, context, step) {
   // writeFileAsync unescapes so need to double escape
   script = script.replaceAll('\\', '\\\\');
 
+  const portScriptsCode = portScripts(context.pipelinepath + '/' + step.id);
+
   const interpreted = `${debugcode}
 const params = { ${paramNodeDef} };
 
-if (!fs.existsSync('./hal9__apis/')) fs.mkdirSync('./hal9__apis/');
-const portsfile = './hal9__apis/runtime.json';
-const portsidentifier = '${context.pipelinepath}/${step.id}';
-console.log('Ports identifier: ' + portsidentifier)
-
-const getAllPorts = () => {
-  try {
-    const rawports = fs.readFileSync(portsfile);
-    return JSON.parse(rawports);
-  } catch(e) {
-    console.log('Error reading ports file: ' + e.toString())
-    fs.writeFileSync(portsfile,  JSON.stringify({}));
-    return {};
-  }
-}
-
-const getNextPort = () => {
-  const alldata = getAllPorts();
-  const ports = Object.keys(alldata).map(e => alldata[e].port ? alldata[e].port : 0);
-  return Math.max(8000, Math.max(...ports) + 1)
-}
-
-const updatePortsFile = (data) => {
-  // data: hash, pid, port
-  let portsinfo = getAllPorts();
-  portsinfo[portsidentifier] = portsinfo[portsidentifier] ? portsinfo[portsidentifier] : {};
-  portsinfo[portsidentifier] = Object.assign(portsinfo[portsidentifier], data);
-  fs.writeFileSync(portsfile,  JSON.stringify(portsinfo));
-}
-
-const getPortsFile = () => {
-  let portsdata = {}
-  let portsinfo = getAllPorts();
-  if (portsinfo[portsidentifier]) portsdata = portsinfo[portsidentifier];
-  return portsdata;
-}
-
-const getPortNumber = () => {
-  const portsdata = getPortsFile();
-  return portsdata.port ? portsdata.port : getNextPort();
-}
-
-const portnumber = getPortNumber();
+${portScriptsCode}
 
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
