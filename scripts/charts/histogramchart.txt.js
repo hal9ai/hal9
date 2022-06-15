724fd2ccd8992612d30f84cae7c6f860f28af114
anchit-sadana <asadana@berkeley.edu>
2022-06-15 11:05:47 -0700

    update histogram to use plotly

--- scripts/charts/histogramchart.txt.js
+++ scripts/charts/histogramchart.txt.js
@@ -1,29 +1,65 @@
 /**
-  output: [html]
+  output: [console,html]
   params:
-    - name: x
-      label: x
-    - name: color
-      label: color
-    - name: thresholds
-      label: Thresholds
-      value:
-        - control: range
-          value: 20
-          min: 0
-          max: 100
-    - name: orientation
-      label: Orientation
+    - x
+    - name: histnorm
+      label: Normalization
       value:
         - control: select
-          value: vertical
+          value: none
           values:
-            - name: vertical
-              label: Vertical
-            - name: horizontal
-              label: Horizontal
+            - name: 'probability density'
+              label: 'Probability Density'
+            - name: percent
+              label: Percentage
+            - name: 'none'
+              label: 'None'
+            - name: percent
+              label: Percent
+            - name: probability
+              label: Probability
+            - name: density
+              label: Density
+    - name: histfunc
+      label: 'Aggregation Function'
+      value:
+        - control: select
+          value: count
+          values:
+            - name: 'count'
+              label: 'Count'
+            - name: min
+              label: Minimum
+            - name: max
+              label: Maximum
+            - name: sum
+              label: Sum
+            - name: probability
+              label: Probability
+            - name: density
+              label: Density
+    - name: barmode
+      label: Bar Mode
+      value:
+        - control: select
+          value: stacked
+          values:
+            - name: stack
+              label: Stacked
+            - name: overlay
+              label: Overlaid
+    - name: dataSizes
+      label: 'Marker Size'
+      value: 
+        - control: 'number'
+          value: 5
+    - name: dataSizes
+      label: 'Marker Size'
+      value: 
+        - control: 'number'
+          value: 5
     - name: palette
-      label: D3 Palette
+      label: Chart Palette
       value:
         - control: paletteSelect
           value: schemeTableau10
@@ -35,96 +71,51 @@
             - schemeSet1
             - schemeSet2
             - schemeSet3
-    - name: fontsize
-      label: Font Size
-      value:
-        - control: range
-          value: 16
-          min: 5
-          max: 20
-    - name: marginleft
-      label: Margin left
-      value:
-        - control: range
-          value: 30
-          min: 20
-          max: 200
-    - name: marginbottom
-      label: Margin Bottom
-      value:
-        - control: range
-          value: 30
-          min: 20
-          max: 200
   deps:
     - https://cdn.jsdelivr.net/npm/hal9-utils@latest/dist/hal9-utils.min.js
-    - https://cdn.jsdelivr.net/npm/d3@6
-    - https://cdn.jsdelivr.net/npm/@observablehq/plot@0.1
+    - https://cdn.plot.ly/plotly-latest.min.js
+    - https://cdn.jsdelivr.net/npm/arquero@latest
 **/
 
-data = await hal9.utils.toRows(data);
-
-if (data.length > 10000) {
-  throw(`Up to 10,000 data points are supported for this visualization, but ${data.length} were provided.`);
+if (data == null) {
+  throw 'No input data defined. Please use an import block to import data before visualizing it.'
 }
-
-const chartdata = x
-  ? data.map(v => ({ x: hal9.utils.convert(v[x]), color: hal9.utils.convert(v[color]) }))
-  : [];
-
-const barAxis = orientation === 'vertical' ? 'x' : 'y';
-const valueAxis = orientation === 'vertical' ? 'y' : 'x';
-
-const barAxisName = barAxis.toUpperCase();
-const valueAxisName = valueAxis.toUpperCase();
-
-const getTitle = d => {
-  const color = d[0].color;
-  const title =
-    (color ? `name: ${typeof color === 'string' ? color.toLocaleString() : color}\n` : '') +
-    `count: ${d.length}`;
-
-  return title;
+data = await hal9.utils.toArquero(data);
+if (x == null){
+throw 'Please select a column for the x-axis'
 }
-
-const colors = [...new Set(chartdata.map(d => d.color))].filter(Boolean).sort((a, b) => a - b);
-const legend = hal9.utils.createLegend({
-  names: colors,
-  colors: d3[palette],
-});
-const marks = x ? [
-  Plot[`rect${valueAxisName}`](chartdata, Plot[`bin${barAxisName}`](
-    { [valueAxis]: 'count' },
-    {
-      [barAxis]: 'x',
-      thresholds: parseInt(thresholds),
-      fill: color ? 'color' : d3[palette][0],
-      title: d => getTitle(d),
-    }
-  )),
-] : [];
-const plot = Plot.plot({
-  marks,
-  [valueAxis]: {
-    grid: true,
-    tickFormat: (d) => (d > 1000 ? d3.format('~s')(d) : d),
-  },
-  color: {
-    range: d3[palette],
-  },
-  width: html.clientWidth,
-  height: html.clientHeight,
-  marginTop: 30,
-  marginLeft: parseInt(marginleft),
-  marginBottom: parseInt(marginbottom),
-  style: {
-    background: hal9.isDark() ? '#222' : '',
-    color: hal9.isDark() ? 'white' : '',
-    fontSize: parseInt(fontsize),
-  },
-});
-
-legend.style.color = hal9.isDark() ? 'white' : '';
-
-html.appendChild(legend);
-html.appendChild(plot);
+var layout = {
+title: x + ' Histogram',
+xaxis: {
+  title: x,
+  showgrid: false,
+  zeroline: false
+},
+yaxis: {
+          showline: false
+},
+width: html.offsetWidth,
+barmode: barmode
+}
+if (histnorm == 'none'){
+layout['yaxis']['title'] = histfunc
+} else {
+layout['yaxis']['title'] = histnorm
+}
+x = x.constructor === Array ? x : [x];
+chartdata = [];
+for (i=0; i<x.length; i++) {
+var trace = {
+  x: data.array(x[i]),
+  type: "histogram",
+  histnorm: histnorm,
+  histfunc: histfunc,
+  name: x[i],
+  color: palette[i]
+};
+if (barmode === 'overlay' && x.length > 1) {
+  trace['opacity'] = 0.6
+}
+chartdata.push(trace)
+}
+Plotly.newPlot(html, chartdata, layout);
