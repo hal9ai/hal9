05ddd4fde9eac698a6c6b716611b0882d339c583
Javier Luraschi <jluraschi@gmail.com>
2022-06-15 08:01:37 -0700

    fix file input control

--- scripts/controls/inputfile.txt.js
+++ scripts/controls/inputfile.txt.js
@@ -8,29 +8,31 @@ var inputfile = '';
 
 var state = hal9.getState();
 state = state ? state : {};
-html.style.height = '40px';
-
-const input = document.createElement('input');
-input.type = 'file';
-const urldata = document.createElement('input');
-
-
 
 if (state.inputfile) {
-    inputfile = state.inputfile;
-    urldata.value = inputfile;
+  inputfile = state.inputfile;
 }
 
-input.onchange = function (e) {
+if (html.innerHTML == '') {
+  const input = document.createElement('input');
+  input.type = 'file';
+
+  if (state.inputfile) {
+    inputfile = state.inputfile;
+  }
+
+  input.onchange = function (e) {
     const reader = new FileReader()
     reader.onload = async (e) => {
-        const text = (e.target.result);
-        state.inputfile = text
-        hal9.setState(state);
-        hal9.invalidate();
+      const text = (e.target.result);
+      state.inputfile = text
+      hal9.setState(state);
+      hal9.invalidate();
     };
     reader.readAsDataURL(e.target.files[0])
+  }
+
+  html.appendChild(input);
 }
-html.appendChild(input);
 
-
+html.style.height = '40px';
