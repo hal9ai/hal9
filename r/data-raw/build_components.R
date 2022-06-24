27334883fa05fccadf4b93573054ea88d4749210
Fernando <fptcorrea@gmail.com>
2022-06-24 18:02:28 -0300

    build components

---
+++ r/data-raw/build_components.R
@@ -0,0 +1,18 @@
+pkgload::load_all()
+
+components_file <- "../scripts/components.json"
+
+components <- jsonlite::read_json(components_file) |>
+  unlist(recursive = FALSE, use.names = FALSE)
+
+for (i in 1:length(components)) {
+  script <- paste0("../scripts/", components[[i]]$source)
+  if (grepl("\\.js$", script)) {
+    list <- script |>
+      parse_txt_js()
+    components[[i]]$params <- list$params
+    components[[i]]$deps <- list$deps
+  }
+}
+
+usethis::use_data(components, internal = TRUE, overwrite = TRUE)
