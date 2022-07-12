14dc9c702fe843cb76776727f6a47524c098778c
Javier Luraschi <jluraschi@gmail.com>
2022-07-12 01:07:40 -0700

    add support for interactive mode in html blocks

--- src/core/interpreters/html.js
+++ src/core/interpreters/html.js
@@ -5,84 +5,102 @@ export default async function(html, header, context) {
 
   const output = header.output ? header.output : [ 'data' ];
   const outputdict = output.filter(e => e != 'html').map((e) => e + ': ' + e).join(', ');
+  const outputnoninteractive = output.map(e => e + ' = result.' + e).join('\n');
 
-  var script = debugcode + '\nif (html) html.innerHTML = `' + html.replace(/`/g, '\\`').replace(/\${/g, '\\${') + '`;\n\n';
+  var outputinteractive = '';
+  var interactiveCheck = '';
+  if (header.interactive) {
+    interactiveCheck = " && html.innerHTML == ''"
+    outputinteractive = output.map(e => e + ' = state.' + e).join('\n');
+  }
+
+  var script = debugcode + '\n' +
+  'var hal9ModifiedHtml = false;\n' +
+  'if (html' + interactiveCheck + ') {\n' +
+  '  hal9ModifiedHtml = true;\n' +
+  '  html.innerHTML = `' + html.replace(/`/g, '\\`').replace(/\${/g, '\\${') + '`;\n' +
+  '}';
 
   script +=  `
-    const scripts = [...html.getElementsByTagName('script')];
     var result = null;
+    if (hal9ModifiedHtml) {
+      const scripts = [...html.getElementsByTagName('script')];
 
-    var hasUnknownType = false;
-    for (var idx in scripts) {
-      const script = scripts[idx];
+      var hasUnknownType = false;
+      for (var idx in scripts) {
+        const script = scripts[idx];
 
-      var tag = document.createElement("script");
+        var tag = document.createElement("script");
 
-      script.getAttributeNames().forEach(function(name) {
-        if (name != 'id') tag.setAttribute(name, script.getAttribute(name));
-      });
-
-      if (script.src || script.type == 'module') {
-        if (script.type == 'module')
-          tag.innerHTML = script.innerHTML + ';hal9ScriptLoaded();';
-        else
-          tag.src = script.src;
-
-        script.remove();
-
-        if (!window.hal9scripts) window.hal9scripts = {};
-        if (script.src && window.hal9scripts[script.src] === 'loaded') continue;
-        
-        const loaded = new Promise((resolve, reject) => {
-          const errorHandler = function(e) {
-            window.removeEventListener('error', errorHandler);
-            reject(e.message);
-          }
-
-          const loadHandler = function() {
-            window.removeEventListener('error', errorHandler);
-            if (script.src) window.hal9scripts[script.src] = 'loaded'
-            resolve();
-          }
-
-          window.addEventListener('error', errorHandler);
-          window.hal9ScriptLoaded = loadHandler;
-
-          tag.addEventListener('load', loadHandler);
-          tag.addEventListener('error', errorHandler);
-          tag.addEventListener('abort', errorHandler);   
+        script.getAttributeNames().forEach(function(name) {
+          if (name != 'id') tag.setAttribute(name, script.getAttribute(name));
         });
 
-        html.appendChild(tag);
+        if (script.src || script.type == 'module') {
+          if (script.type == 'module')
+            tag.innerHTML = script.innerHTML + ';hal9ScriptLoaded();';
+          else
+            tag.src = script.src;
 
-        await loaded;
-      }
-      else if (!script.type || script.type == 'text/javascript' || script.type == 'text/jsx' || script.type == 'text/jsx' || script.type == 'text/babel') {
-        var code = script.innerHTML;
+          script.remove();
 
-        if (!script.type || script.type == 'text/javascript') {
-          code = code + "\\n" +
-            "return { ${outputdict} };"
+          if (!window.hal9scripts) window.hal9scripts = {};
+          if (script.src && window.hal9scripts[script.src] === 'loaded') continue;
+          
+          const loaded = new Promise((resolve, reject) => {
+            const errorHandler = function(e) {
+              window.removeEventListener('error', errorHandler);
+              reject(e.message);
+            }
+
+            const loadHandler = function() {
+              window.removeEventListener('error', errorHandler);
+              if (script.src) window.hal9scripts[script.src] = 'loaded'
+              resolve();
+            }
+
+            window.addEventListener('error', errorHandler);
+            window.hal9ScriptLoaded = loadHandler;
+
+            tag.addEventListener('load', loadHandler);
+            tag.addEventListener('error', errorHandler);
+            tag.addEventListener('abort', errorHandler);   
+          });
+
+          html.appendChild(tag);
+
+          await loaded;
         }
+        else if (!script.type || script.type == 'text/javascript' || script.type == 'text/jsx' || script.type == 'text/jsx' || script.type == 'text/babel') {
+          var code = script.innerHTML;
 
-        if (script.type == 'text/jsx' || script.type == 'text/babel')
-          code = Babel.transform(code, { presets: ['env', 'react'] }).code;
+          if (!script.type || script.type == 'text/javascript') {
+            code = code + "\\n" +
+              "return { ${outputdict} };"
+          }
 
-        const fn = new Function(Object.keys(_hal9_params), code);
+          if (script.type == 'text/jsx' || script.type == 'text/babel')
+            code = Babel.transform(code, { presets: ['env', 'react'] }).code;
 
-        if (!result)
-          result = await fn(...Object.values(_hal9_params));
+          const fn = new Function(Object.keys(_hal9_params), code);
+
+          if (!result)
+            result = await fn(...Object.values(_hal9_params));
+
+          ${outputnoninteractive}
+        }
+        else {
+          hasUnknownType = true;
+        }
+      };
+
+      if (hasUnknownType) {
+        window.dispatchEvent(new Event('DOMContentLoaded'));
       }
-      else {
-        hasUnknownType = true;
-      }
-    };
-
-    if (hasUnknownType) {
-      window.dispatchEvent(new Event('DOMContentLoaded'));
+    } else {
+      const state = hal9.getState();
+      ${outputinteractive}
     }
-
-    return result ? result : {};
   `;
 
   return {
