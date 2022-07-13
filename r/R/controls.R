bf487d8d51bc7e5696a5d27e692074fff5ca074c
Javier Luraschi <jluraschi@gmail.com>
2022-07-13 13:16:18 -0500

    rebuild package

--- r/R/controls.R
+++ r/R/controls.R
@@ -17,3 +17,43 @@ h9_dropdown <- function(h, values  = NULL, ...) {
   )
 }
 
+#' Slider
+#'
+#' Embed an slider element
+#'
+#' @param h A h9 object created by h9_create.
+#' @param min Additional step parameter.
+#' @param max Additional step parameter.
+#' @param step Additional step parameter.
+#' @param ... Other h9 parameters.
+#'
+#' @return A list with the pipeline specification.
+#' @export
+#'
+h9_slider <- function(h, min  = NULL, max  = NULL, step  = NULL, ...) {
+  h9_add_step(
+    h,
+    "slider",
+    matched_call = as.list(match.call())
+  )
+}
+
+#' Website
+#'
+#' Embed a website and load a URL
+#'
+#' @param h A h9 object created by h9_create.
+#' @param site Additional step parameter.
+#' @param ... Other h9 parameters.
+#'
+#' @return A list with the pipeline specification.
+#' @export
+#'
+h9_website <- function(h, site  = NULL, ...) {
+  h9_add_step(
+    h,
+    "website",
+    matched_call = as.list(match.call())
+  )
+}
+
