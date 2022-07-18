ede9bab5fb4cbad3a2899c7c668ad0beeb31bc30
kevinykuo <kevinykuo@gmail.com>
2022-07-18 19:34:38 +0000

    remove logging

--- R/bussin.R
+++ R/bussin.R
@@ -31,10 +31,8 @@ register_node <- function(obj, uid) {
 #' @export 
 bs_dropdown <- function(uid, values, on_update = NULL) {
     values <- maybe_convert_to_fn(values)
-    # default_value <- values()[[1]]
     on_update <- maybe_convert_to_fn(on_update)
     node <- Node$new(uid, "dropdown", values, list(on_update = on_update))
-    # node$evaluate("on_update", default_value)
     invisible(NULL)
 }
 
@@ -56,7 +54,6 @@ maybe_convert_to_fn <- function(x) {
 
 #' @export 
 bs_set <- function(value, name) {
-    cat(glue::glue("setting `{name}` to `{value}`"), "\n")
     .globals_data[[name]] <- value
     invisible(value)
 }
@@ -74,27 +71,17 @@ get_node <- function(uid) {
 process_request <- function(req) {
     uids <- names(req)
 
-    cat(glue::glue("the nodes to be processed are {paste0(uids, collapse = ', ')}"), "\n")
-
     lapply(uids, function(uid) {
-        cat(glue::glue("now in uid {uid}"), "\n")
         node <- get_node(uid)
-        cat("get node ", uid, " successful", "\n")
         fn_args <- req[[uid]]
-        # cat(glue::glue("fn_args for {uid} is {unlist(fn_args)}"), "\n")
-        cat("fn_args is of length ", length(fn_args), "\n")
 
         if (!length(fn_args)) {
             fn <- list("main")
             .args <- NULL
-            cat(glue::glue("fn is main and args is null"), "\n")
         } else {
             fn <- names(fn_args)
             .args <- unname(fn_args)
-            cat(glue::glue("fn is {fn}"), "\n")
         }
-
-        cat(glue::glue("running node {uid} function {fn} with args {paste0(.args, collapse = '')}"), "\n")
 
         list(
             result = do.call(node$evaluate, c(fn = fn, .args)),
