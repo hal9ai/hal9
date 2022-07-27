e43525c10c01bfff00f96c0020d91b52cfe9a884
Fernando Corrêa <fptcorrea@gmail.com>
2022-07-27 14:56:27 -0300

    implement hal9 class with print and inclusion methods

--- python/src/hal9/__init__.py
+++ python/src/hal9/__init__.py
@@ -1,7 +1,10 @@
 import hal9
+import pkg_resources
 
 from IPython.core.display import display, HTML
 import json
+
+json.load(open("D:\hal9ai5\scripts\import\json.json"))
 
 def encode(data):
   if (type(data).__name__ == "DataFrame"):
@@ -9,7 +12,37 @@ def encode(data):
   else:
     return json.dumps(data)
 
-def create(data, height = 400, **kwargs):
+class hal9:
+  """hal9 pipeline class"""
+
+  def __init__(self):
+    self.params = []
+    self.outputs = []
+    self.steps = []
+
+  def add_step(self, step_name):
+
+    my_file = open(pkg_resources.resource_filename('hal9', 'data/'+step_name+'.txt'))
+    step_data = my_file.readlines()
+
+  def show(self, height = 400, **kwargs):
+    display(HTML("""<script>
+    window.hal9 = {
+      data: "",
+      pipeline: {
+        "steps": """ + self.steps + """",
+        "params": """ + self.params + """",
+        "outputs": """ + self.outputs + """",
+        scripts": { "1": "data = window.hal9.data" },
+        "version": "0.0.1"
+      }
+    }
+    </script>
+    <script defer src="https://hal9.com/hal9.notebook.js"></script>
+    <div style='width: 100%; padding: 6px; height: """ + str(height) + "px'><div id='app'></div></div>"
+    ))
+
+def show(data, height = 400, **kwargs):
   display(HTML("""<script>
     window.hal9 = {
       data: """ + encode(data) + """,
