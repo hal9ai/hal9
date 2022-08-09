1f7261c4e32229bb36c905bb76866e7b7cff31a1
anchit-sadana <asadana@berkeley.edu>
2022-08-09 12:06:50 -0700

    add type hints to python

--- py/src/bussin.py
+++ py/src/bussin.py
@@ -1,13 +1,15 @@
 import json
+from typing import Callable, Any
 
 class _Node:
+    """
+    The base class which defines the backend execution graph
+    """
     def __init__(self, uid:str=  None, kind:str = None, **funcs) -> None:
         self.uid = uid
         self.type = kind
         self.funcs = funcs
         _register_node(self)
-    # def __register_node(self):
-    #     _register_node(self)
     
     def evaluate(self, fn, fn_args):
         return self.funcs[fn](*fn_args)
@@ -18,17 +20,20 @@ global_data = dict()
 def _register_node(node: _Node) -> None:
     global_nodes[node.uid] = node
     
-def dropdown(uid, values, on_update = None) -> _Node:
-    return _Node(uid, kind = 'Dropdown', main = values, on_update=on_update)
+def dropdown(name: str, values: Callable[..., list], on_update = None) -> _Node:
+    """
+    Write the 
+    """
+    return _Node(name, kind = 'dropdown', main = values, on_update=on_update)
 
 def get(x: str) -> _Node:
     return global_data[x]
 
-def set(name: str, value) -> None:
+def set(name: str, value: Any) -> None:
     global_data[name] = value
     return value
 
-def code(uid, codefunc) -> _Node:
+def code(uid: str, codefunc: Callable[..., str]) -> _Node:
     return _Node(uid = uid, kind = 'pycode', main = codefunc)
     
 def __process_request(request: dict) -> None:
@@ -47,9 +52,10 @@ def __process_request(request: dict) -> None:
                         'type': node.type}
     return response
 
-def __get_designer(**options) -> str:
+def __get_designer(**options: dict) -> str:
     options = json.dumps(options)
     with open('../../inst/client.html') as f:
         html = f.read()
     html = html.replace("__options__", options)
     return html
+
