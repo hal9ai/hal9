47916f516d82042f1d5bbe55f455588157e7080d
Javier Luraschi <jluraschi@gmail.com>
2022-08-11 15:39:31 -0700

    merge gregs api through inplace option

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import * as native from './native';
 import * as iframe from './iframe';
+import clone from '../core/utils/clone';
 import { launchDesigner } from './designer';
 
-const api = Object.assign(native.init(), {
+var api = Object.assign(native.init(), {
   init: init,
 });
 
@@ -11,18 +12,32 @@ export default api;
 export async function init(options, hal9wnd) {
   hal9wnd = hal9wnd ? hal9wnd : window.hal9;
 
-  for (const key in api) {
-    delete api[key];
+  if (options.inplace) {
+    for (const key in api) {
+      delete api[key];
+    }
+    if (options.iframe) {
+      Object.assign(api, await iframe.init(options, hal9wnd));
+    } else {
+      Object.assign(api, native.init(options, hal9wnd));
+    }
   }
-  if (options.iframe) {
-    Object.assign(api, await iframe.init(options, hal9wnd));
-  } else {
-    Object.assign(api, native.init(options, hal9wnd));
+  else {
+    if (options.iframe) {
+      api = await iframe.init(options, hal9wnd);
+    }
+    else {
+      api = await native.init(options, hal9wnd);
+    }
+
+    if (api) {
+      api = Object.assign(api, {
+        design: async (pid) => {
+          await launchDesigner(api, options, pid)
+        }
+      })
+    }
   }
 
-  Object.assign(api, {
-    design: async (pid) => {
-      await launchDesigner(api, options, pid)
-    }
-  });
+  return api;
 }
