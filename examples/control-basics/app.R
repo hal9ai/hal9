356fd1fbf1c755c7aa7553f4d5efbec42c5874d7
Javier Luraschi <jluraschi@gmail.com>
2022-08-24 22:30:01 +0000

    add control example with two controls

--- examples/control-basics/app.R
+++ examples/control-basics/app.R
@@ -1,1 +1,27 @@
+library(hal9)
 
+iris |> h9_set("df")
+
+h9_node(
+  "numberinput",
+  on_update = function(value) {
+    h9_set(value, "number")
+  }
+)
+
+h9_node(
+  "dropdown",
+  on_update = function(value) {
+    h9_set(value, "dropdown")
+  }
+)
+
+h9_node(
+  "rawhtml",
+  rawhtml = function() {
+    paste0(
+      "Number (", h9_get("number"), ") <br>",
+      "Drodown (", h9_get("dropdown"), ") <br>"
+    )
+  }
+)
