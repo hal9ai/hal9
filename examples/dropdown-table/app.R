699f0fcbdd1b8ef61a24158b29ed28c034a0768c
Javier Luraschi <jluraschi@gmail.com>
2022-08-24 22:29:24 +0000

    fix dropdown-table example

--- examples/dropdown-table/app.R
+++ examples/dropdown-table/app.R
@@ -21,26 +21,3 @@ h9_node(
       as.character()
   }
 )
-library(hal9)
-
-iris |> h9_set("df")
-
-h9_node(
-  "dropdown",
-  values = c("setosa", "versicolor"),
-  on_update = function(value) {
-    h9_set(value, "selected_species")
-  }
-)
-
-h9_node(
-  "rawhtml",
-  rawhtml = function() {
-    df <- h9_get("df")
-    selected_species <- h9_get("selected_species")
-    df <- df[df$Species == selected_species, ]
-
-    knitr::kable(df, format = "html") |>
-      as.character()
-  }
-)
