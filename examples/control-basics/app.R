137893b234ed4045d4e86b5d0225eb0e2d3d0861
Javier Luraschi <jluraschi@gmail.com>
2022-08-25 22:32:56 +0100

    add more controls to control-basics example

--- examples/control-basics/app.R
+++ examples/control-basics/app.R
@@ -17,11 +17,35 @@ h9_node(
 )
 
 h9_node(
+  "textbox",
+  on_update = function(value) {
+    h9_set(value, "textbox")
+  }
+)
+
+h9_node(
+  "textarea",
+  on_update = function(value) {
+    h9_set(value, "textarea")
+  }
+)
+
+h9_node(
+  "slider",
+  on_update = function(value) {
+    h9_set(value, "slider")
+  }
+)
+
+h9_node(
   "rawhtml",
   rawhtml = function() {
     paste0(
       "Number (", h9_get("number"), ") <br>",
-      "Drodown (", h9_get("dropdown"), ") <br>"
+      "Drodown (", h9_get("dropdown"), ") <br>",
+      "Textbox (", h9_get("textbox"), ") <br>",
+      "Textarea (", h9_get("textarea"), ") <br>",
+      "Slider (", h9_get("slider"), ") <br>"
     )
   }
 )
