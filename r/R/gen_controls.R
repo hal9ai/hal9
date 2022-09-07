d4cb93c5865bea0d41b34a7a1b418eab90070746
Javier Luraschi <jluraschi@gmail.com>
2022-09-07 09:23:14 -0700

    rebuild r package

--- r/R/gen_controls.R
+++ r/R/gen_controls.R
@@ -1,6 +1,6 @@
 #' Dropdown
 #'
-#' Embed an HTML <select> element
+#' Embed a dropdown control
 #'
 #' @param h An optional h9 object created by h9_create.
 #' @param values Additional step parameter.
@@ -37,14 +37,14 @@ h9_file <- function(uid, dataType  = NULL, ...) {
 #' Embed a number input control
 #'
 #' @param h An optional h9 object created by h9_create.
-#' @param paramName Additional step parameter.
+#' @param label Additional step parameter.
 #' @param uid Unique identifier for this element.
 #' @param ... Other h9 parameters.
 #'
 #' @return A list with the pipeline specification.
 #' @export
 #'
-h9_number <- function(uid, paramName  = NULL, ...) {
+h9_number <- function(uid, label  = NULL, ...) {
   node_args <- args[!names(args) %in% c("h", "", "uid")]
   do.call("h9_node", args = c(uid, node_args))
 }
@@ -83,6 +83,23 @@ h9_slider <- function(uid, value  = NULL, min  = NULL, max  = NULL, step  = NULL
 #' @export
 #'
 h9_textbox <- function(uid, label  = NULL, value  = NULL, ...) {
+  node_args <- args[!names(args) %in% c("h", "", "uid")]
+  do.call("h9_node", args = c(uid, node_args))
+}
+
+#' Website
+#'
+#' Embed a website and load a URL
+#'
+#' @param h An optional h9 object created by h9_create.
+#' @param site Additional step parameter.
+#' @param uid Unique identifier for this element.
+#' @param ... Other h9 parameters.
+#'
+#' @return A list with the pipeline specification.
+#' @export
+#'
+h9_website <- function(uid, site  = NULL, ...) {
   node_args <- args[!names(args) %in% c("h", "", "uid")]
   do.call("h9_node", args = c(uid, node_args))
 }
