db2757459d3cb14634515951bf46bd76e49a6bf0
kevinykuo <kevinykuo@gmail.com>
2022-09-07 10:29:16 -0700

    allow user port selection in r api

--- server/src/extendr.rs
+++ server/src/extendr.rs
@@ -6,10 +6,10 @@ use extendr_api::prelude::*;
 /// Start server.
 /// @export
 #[extendr]
-fn h9_start2(#[default = "."] path: String) -> u32 {
+fn h9_start2(#[default = "."] path: String, #[default = "NA_integer_"] port: Option<i32>) {
     // #[allow(clippy::not_unsafe_ptr_arg_deref)]
-    start_server(path, 0).ok();
-    0
+    let port: u16 = port.unwrap_or(0).try_into().unwrap();
+    start_server(path, port).ok();
 }
 
 // Macro to generate exports.
