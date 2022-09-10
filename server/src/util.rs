5498eda039291867a2b7dc7f194156db424c4c11
kevinykuo <kevinykuo@gmail.com>
2022-09-09 17:42:29 -0700

    refactor process shutdown ceremony

--- server/src/util.rs
+++ server/src/util.rs
@@ -1,77 +1,97 @@
-use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::mpsc::channel;
-use std::sync::mpsc::Sender;
+use tokio::sync::mpsc::Sender;
 use std::sync::Arc;
-use std::time::Duration;
 use std::time::SystemTime;
-
+use crate::shutdown::Shutdown;
+use tokio::sync::mpsc;
+use crate::async_watcher::async_watcher;
 use crate::runtimes::RtControllerMsg;
+use notify::{RecursiveMode, Watcher};
+use notify::event::EventKind;
+use futures::StreamExt;
 
-pub fn monitor_heartbeat(
+pub(crate) fn monitor_heartbeat(
     handle: actix_web::dev::ServerHandle,
     last_heartbeat_ref: Arc<AtomicUsize>,
     timeout_secs: u32,
     runtime_controller_tx: Sender<RtControllerMsg>,
-) {
-    tokio::spawn(async move {
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(1000));
-        loop {
-            interval.tick().await;
-            let last_heartbeat: u32 = last_heartbeat_ref
-                .load(Ordering::SeqCst)
-                .try_into()
-                .unwrap();
-            let diff: u32 = time_now() - last_heartbeat;
-            if diff > timeout_secs {
-                println!(
-                    "no heartbeat from client for {}s, shutting down",
-                    timeout_secs
-                );
-                runtime_controller_tx
-                    .send(RtControllerMsg::StopAll)
-                    .unwrap();
-                handle.stop(true).await;
-            }
-        }
-    });
-}
-
-pub async fn monitor_fs_changes(
-    dir: String,
-    interval_ms: u64,
-    runtime_controller_tx: Sender<RtControllerMsg>,
+    mut shutdown: Shutdown,
+    _shutdown_complete_tx: mpsc::Sender<()>,
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
-        let (tx, rx) = channel();
-        let mut watcher = watcher(tx, Duration::from_millis(interval_ms)).unwrap();
-
-        watcher.watch(dir, RecursiveMode::Recursive).unwrap();
-
-        loop {
-            let msg = rx.recv();
-            match msg {
-                Ok(event) => match event {
-                    DebouncedEvent::Write(_) => {
-                        println!("write event detected, restarting runtimes");
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(1000));
+        while !shutdown.is_shutdown() {
+            tokio::select! {
+                _ = interval.tick() => {
+                    let last_heartbeat: u32 = last_heartbeat_ref
+                    .load(Ordering::SeqCst)
+                    .try_into()
+                    .unwrap();
+                    let diff: u32 = time_now() - last_heartbeat;
+                    if diff > timeout_secs {
+                        println!(
+                            "no heartbeat from client for {}s, shutting down",
+                            timeout_secs
+                        );
                         runtime_controller_tx
-                            .send(RtControllerMsg::RestartAll)
-                            .unwrap();
-                    }
-                    _ => println!("{:?}", event),
-                },
-
-                Err(e) => println!("watch error: {:?}", e),
+                        .send(RtControllerMsg::StopAll)
+                        .await.ok();
+                        handle.stop(true).await;
+                        break;
+                    } 
+                }
+                _ = shutdown.recv() => {
+                    println!("heartbeat monitor shutting down");
+                }
             }
         }
+        
     })
 }
 
+#[allow(clippy::collapsible_match)]
+pub(crate) fn monitor_fs_changes(
+    dir: String,
+    runtime_controller_tx: Sender<RtControllerMsg>,
+    mut shutdown: Shutdown,
+    _shutdown_complete_tx: mpsc::Sender<()>,
+) -> tokio::task::JoinHandle<Result<(), notify::Error>> {
+    
+    tokio::spawn( async move {
+        let (mut watcher, mut rx) = async_watcher()?;
+        
+        watcher.watch(dir.as_ref(), RecursiveMode::Recursive)?;
+        while !shutdown.is_shutdown() {
+            tokio::select! {
+                msg = rx.next() => {
+                    if let Some(Ok(event)) = msg {
+                        if let EventKind::Modify(kind) = event.kind {
+                            match kind {
+                                notify::event::ModifyKind::Data(_x) => {
+                                    let paths = event.paths;
+                                    println!("detected data modification in {paths:?}");
+                                    runtime_controller_tx.send(RtControllerMsg::RestartAll).await.ok();
+                                }
+                                _ => {}
+                            } 
+                        }
+                    }
+                }
+                _ =  shutdown.recv() => {
+                }
+            }
+        }
+        
+        notify::Result::Ok(())
+    })
+}
+
+
 pub fn time_now() -> u32 {
     SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
-        .try_into()
-        .unwrap()
+    .duration_since(SystemTime::UNIX_EPOCH)
+    .unwrap()
+    .as_secs()
+    .try_into()
+    .unwrap()
 }
