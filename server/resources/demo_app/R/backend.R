2d6126f0cc36d97979d89dd031965072acb9b5e8
kevinykuo <kevinykuo@gmail.com>
2022-09-16 00:41:26 -0700

    move template to resources

---
+++ server/resources/demo_app/R/backend.R
@@ -0,0 +1,43 @@
+library(hal9)
+
+iris |> h9_set("df")
+
+h9_dropdown(
+    "species_dropdown",
+    values = unique(levels(iris$Species)),
+    on_update = function(value) {
+        h9_set(value, "selected_species")
+    }
+)
+
+h9_rawhtml(
+    "iris_table",
+    rawhtml = function() {
+        sp <- h9_get("selected_species")
+        if (is.null(sp)) {
+            return("")
+        }
+
+        out <- iris[iris$Species == sp, ]
+
+        num_rows <- h9_get("num_rows")
+
+        if (!is.null(num_rows)) {
+            out <- out |> head(num_rows)
+        }
+
+        out |>
+            knitr::kable(format = "html") |>
+            as.character()
+    }
+)
+
+h9_slider("slider_rows",
+    value = 1,
+    min = 1,
+    max = 10,
+    step = 1,
+    on_update = function(value) {
+        h9_set(value, "num_rows")
+    }
+)
