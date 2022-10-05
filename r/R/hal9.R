dcd169e7ba9c6dd6234ad5d0e6a44fae2786a958
kevinykuo <kevinykuo@gmail.com>
2022-10-04 22:27:14 -0700

    try in h9_run_script()

--- r/R/hal9.R
+++ r/R/hal9.R
@@ -10,7 +10,7 @@ Node <- R6::R6Class("Node", list(
         register_node(self, self$uid)
         self
     },
-    evaluate = function(fn, ...) {
+    evaluate = function(fn, ..., env) {
         fn <- self$fns[[fn]]
 
         if (is.null(fn)) {
@@ -65,6 +65,7 @@ get_node <- function(uid) {
 }
 
 process_request <- function(req) {
+    env <- parent.frame()
     responses <- lapply(req, function(call) {
         node <- get_node(call$node)
         fn_name <- call$fn_name
@@ -84,7 +85,7 @@ process_request <- function(req) {
         fn_args <- setNames(fn_args_values, fn_args_names)
 
         result <- tryCatch(
-            node$evaluate(fn_name, fn_args),
+            node$evaluate(fn_name, fn_args, env = env),
             error = function(e) paste0(e, collapse = "\\n")
         )
 
@@ -113,23 +114,28 @@ client_html <- function(...) {
 }
 
 h9_run_script <- function(app = "app.R", port = NULL) {
-    if (!file.exists(app)) writeLines("", app)
+    tryCatch(
+        {
+            if (!file.exists(app)) writeLines("", app)
 
-    user_code <- readLines(app)
-    server_code <- readLines(system.file("server-spec.R", package = "hal9"))
+            user_code <- readLines(app)
+            server_code <- readLines(system.file("server-spec.R", package = "hal9"))
 
-    api_file <- tempfile()
-    writeLines(c(
-        "## User Code",
-        user_code,
-        "",
-        "## Hal9 Code",
-        paste0("app_file <- \"", normalizePath(app, winslash = "/"), "\""),
-        paste0("app_path <- \"", dirname(normalizePath(app)), "\""),
-        "",
-        server_code
-    ), api_file)
+            api_file <- tempfile()
+            writeLines(c(
+                "## User Code",
+                user_code,
+                "",
+                "## Hal9 Code",
+                paste0("app_file <- \"", normalizePath(app, winslash = "/"), "\""),
+                paste0("app_path <- \"", dirname(normalizePath(app)), "\""),
+                "",
+                server_code
+            ), api_file)
 
-    pb <- plumber::plumb(api_file)
-    plumber::pr_run(pb, port = port)
+            pb <- plumber::plumb(api_file)
+            plumber::pr_run(pb, port = port)
+        },
+        error = function(e) message("runtime_startup_error:", paste0(e, collapse = "\n"))
+    )
 }
