8c0252e93097fd1c1a9433f27b662d4db443fbd5
Anchit Sadana <70300199+anchit-sadana@users.noreply.github.com>
2022-10-07 13:15:39 -0700

    feature/rustincorporation (#197)
    * add maturin to poetry
    
    * finish integrating rust with python

---
+++ server/src/_hal9.rs
@@ -0,0 +1,19 @@
+use pyo3::prelude::*;
+use crate::app_template::new_app;
+use crate::server::start_server;
+
+#[pyfunction(path = "\".\"", port = "0", timeout = "600", nobrowse = "false")]
+fn start(path: &str, port: u16, timeout: u32, nobrowse: bool){
+    start_server(path.to_string(), port, timeout, nobrowse).ok();
+}
+#[pyfunction(path = "\".\"")]
+fn new(path: &str) {
+    new_app(path.to_string()).ok();
+}
+#[pymodule]
+#[pyo3(name = "_hal9")]
+fn _hal9(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(start, m)?)?;
+    m.add_function(wrap_pyfunction!(new, m)?)?;
+    Ok(())
+}
