3c0179923f5fcd6f3946a5255db99ef6f53c3b17
Javier Luraschi <jluraschi@gmail.com>
2022-10-11 15:20:06 -0700

    add descriptions to all controls and types for codegen

--- client/scripts/controls/rawhtml.js
+++ client/scripts/controls/rawhtml.js
@@ -1,5 +1,9 @@
 /**
-  input: [ rawhtml ]
+  params:
+    - name: rawhtml
+      type: string
+      description: An HTML string with arbitrary HTML to render.
+      label: html
   output: [ html ]
   layout:
     - width: 200px
