cec8144fef0daae6954fef97c9d4454849595fa2
kevinykuo <kevinykuo@gmail.com>
2022-10-11 00:20:44 -0700

    feature gate pyo3 deps

--- server/src/_hal9.rs
+++ server/src/_hal9.rs
@@ -1,15 +1,21 @@
+#[cfg(feature = "pyo3")]
 use pyo3::prelude::*;
 use crate::app_template::new_app;
 use crate::server::start_server;
 
+#[cfg(feature = "pyo3")]
 #[pyfunction(path = "\".\"", port = "0", timeout = "600", nobrowse = "false")]
 fn start(path: &str, port: u16, timeout: u32, nobrowse: bool){
     start_server(path.to_string(), port, timeout, nobrowse).ok();
 }
+
+#[cfg(feature = "pyo3")]
 #[pyfunction(path = "\".\"")]
 fn new(path: &str) {
     new_app(path.to_string(), crate::config::Platform::Python).ok();
 }
+
+#[cfg(feature = "pyo3")]
 #[pymodule]
 #[pyo3(name = "_hal9")]
 fn _hal9(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
