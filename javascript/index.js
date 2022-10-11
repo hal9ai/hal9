bb7387ba774a158900444518742d1acdb861e9bd
Javier Luraschi <jluraschi@gmail.com>
2022-10-10 19:45:06 -0700

    Add javascript docs using jsdoc (#206)
    * add project for js docs
    
    * add javascript docs

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+/**
+ * @class
+ * @classdesc Provides access to the Hal9 api.
+ * @hideconstructor
+ */
+function h9() {
+  /**
+   * Creates a Node.
+   * @static
+   * @param {string} name - The name of this node.
+   */
+  function node(name) {
+  }
+}
