cd77430f6fb418e0e3c40620eb6966c47eeb4024
kevinykuo <kevinykuo@gmail.com>
2022-10-21 00:42:43 -0700

    define plumber in backend execution environment

--- r/R/hal9.R
+++ r/R/hal9.R
@@ -83,20 +83,22 @@ process_request <- function(req) {
         fn_args_values <- call$args |> lapply(\(x) x$value)
         fn_args <- setNames(fn_args_values, fn_args_names)
 
-        tryCatch({
-            list(
-                node = call$node,
-                fn_name = fn_name,
-                result = node$evaluate(fn_name, fn_args)
-            )
-        },
-        error = function(e) {
-            list(
-                node = call$node,
-                fn_name = fn_name,
-                error = paste0(e, collapse = "\\n")
-            )
-        })
+        tryCatch(
+            {
+                list(
+                    node = call$node,
+                    fn_name = fn_name,
+                    result = node$evaluate(fn_name, fn_args)
+                )
+            },
+            error = function(e) {
+                list(
+                    node = call$node,
+                    fn_name = fn_name,
+                    error = paste0(e, collapse = "\\n")
+                )
+            }
+        )
     })
     list(calls = responses)
 }
@@ -116,28 +118,37 @@ client_html <- function(...) {
     html
 }
 
-h9_run_script <- function(app = "app.R", port = NULL) {
+h9_run_script <- function(app, port = NULL) {
     tryCatch(
         {
-            if (!file.exists(app)) writeLines("", app)
+            if (!file.exists(app)) {
+                stop(paste0(app, "doesn't exist."))
+            }
 
-            user_code <- readLines(app)
-            server_code <- readLines(system.file("server-spec.R", package = "hal9"))
+            source(app)
 
-            api_file <- tempfile()
-            writeLines(c(
-                "## User Code",
-                user_code,
-                "",
-                "## Hal9 Code",
-                paste0("app_file <- \"", normalizePath(app, winslash = "/"), "\""),
-                paste0("app_path <- \"", dirname(normalizePath(app)), "\""),
-                "",
-                server_code
-            ), api_file)
+            app_path <- dirname(normalizePath(app))
 
-            pb <- plumber::plumb(api_file)
-            plumber::pr_run(pb, port = port)
+            p <- plumber::pr() |>
+                plumber::pr_get("/", function() {
+                    client_html(mode = "run")
+                }, serializer = plumber::serializer_html) |>
+                plumber::pr_get("/design", function() {
+                    client_html(mode = "design")
+                }, serializer = plumber::serializer_html) |>
+                plumber::pr_get("/pipeline", function() {
+                    paste0(readLines(file.path(app_path, "")))
+                }, serializer = plumber::serializer_text) |>
+                plumber::pr_post("/pipeline", function(req) {
+                    writeLines(req$postBody, file.path(app_path, "app.json"))
+                    "{}"
+                }, serializer = plumber::serializer_text) |>
+                plumber::pr_post("/eval", function(req) {
+                    parsed <- jsonlite::fromJSON(req$postBody, simplifyDataFrame = FALSE)
+                    process_request(parsed)
+                }, serializer = plumber::serializer_json(auto_unbox = TRUE, null = "null"))
+
+            plumber::pr_run(p, port = port)
         },
         error = function(e) message("runtime_startup_error:", paste0(e, collapse = "\n"))
     )
