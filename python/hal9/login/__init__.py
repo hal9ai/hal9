d67cbf392fee34f45679f25962900274ad0457ad
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-17 23:19:32 +0900

    Set _RELEASE as True

--- python/hal9/login/__init__.py
+++ python/hal9/login/__init__.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 # the component, and True when we're ready to package and distribute it.
 # (This is, of course, optional - there are innumerable ways to manage your
 # release process.)
-_RELEASE = False
+_RELEASE = True
 
 if not _RELEASE:
     _component_func = components.declare_component(
