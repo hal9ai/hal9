0744703e5f39f2d4d3ce644fac8a9745ab6cce41
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-17 22:43:33 +0900

    Update the <Hal9Login /> component to call the API and function as an actual login component frontend

--- python/hal9/login/frontend/src/Hal9Login.tsx
+++ python/hal9/login/frontend/src/Hal9Login.tsx
@@ -1,13 +1,107 @@
-import { useRenderData } from "streamlit-component-lib-react-hooks";
-import React, { useCallback } from "react";
+import { Streamlit } from "streamlit-component-lib";
+import React, { useState, useCallback, useMemo } from "react";
+
+const LOGIN_TOKEN_REQUEST_API_URL = "https://api.hal9.com/api/login";
+const LOGIN_PAGE_URL = "https://api.hal9.com/login";
+const LOGIN_INFO_API_URL = "https://api.hal9.com/api/login";
+
+function getHal9LoginPageUrl(token: string) {
+  return `${LOGIN_PAGE_URL}?token=${token}`;
+}
+
+function getHal9LoginInfoApiUrl(token: string) {
+  return `${LOGIN_INFO_API_URL}?token=${token}`;
+}
+
+async function fetchLoginToken() {
+  const res = await fetch(LOGIN_TOKEN_REQUEST_API_URL, {
+    method: "POST",
+  });
+  const resJson = await res.json();
+  const token = resJson.token;
+  return token;
+}
+
+function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+interface LoginInfo {
+  user: string;
+  photo: string;
+}
+
+function isLoginInfo(obj: any): obj is LoginInfo {
+  return (
+    "user" in obj &&
+    typeof obj["user"] === "string" &&
+    "photo" in obj &&
+    typeof obj["photo"] === "string"
+  );
+}
+
+async function subscribeLoginInfo(token: string): Promise<LoginInfo> {
+  const res = await fetch(getHal9LoginInfoApiUrl(token));
+
+  if (res.status === 502) {
+    return subscribeLoginInfo(token);
+  } else if (res.status !== 200) {
+    console.log(res.statusText);
+    await delay(1000);
+    return subscribeLoginInfo(token);
+  } else {
+    const resJson = await res.json();
+    if ("done" in resJson && resJson["done"] === false) {
+      console.debug("Login not done yet.");
+      await delay(1000);
+      return subscribeLoginInfo(token);
+    } else {
+      if (!isLoginInfo(resJson)) {
+        throw new Error("Invalid login info");
+      }
+      return resJson;
+    }
+  }
+}
+
+async function login(): Promise<LoginInfo> {
+  const token = await fetchLoginToken();
+
+  window.open(getHal9LoginPageUrl(token), "_blank");
+
+  return subscribeLoginInfo(token);
+}
 
 function Hal9Login() {
-  const handleLoginRequest = useCallback(async () => {
-    console.log("Login requested");
+  const [loginInfo, setLoginInfo] = useState<LoginInfo>();
+
+  const handleLoginRequest = useCallback(() => {
+    setLoginInfo(undefined);
+    try {
+      login().then((loginInfo) => {
+        setLoginInfo(loginInfo);
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }, []);
 
-  const renderData = useRenderData();
-  return <button onClick={handleLoginRequest}>Login</button>;
+  setTimeout(() => {
+    Streamlit.setFrameHeight();
+  });
+
+  if (loginInfo === undefined) {
+    return <button onClick={handleLoginRequest}>Login</button>;
+  }
+
+  return (
+    <div>
+      <p>
+        Logged in as <b>{loginInfo.user}</b>
+      </p>
+      <img src={loginInfo.photo} />
+    </div>
+  );
 }
 
 export default Hal9Login;
