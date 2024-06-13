a4ee003ed3ad795638b2aba3fba9e76a0ed6eb70
Javier Luraschi <jluraschi@gmail.com>
2024-06-13 14:06:35 -0700

    add support for input

--- python/hal9/__init__.py
+++ python/hal9/__init__.py
@@ -1,5 +1,5 @@
 from hal9.create import create
 from hal9.run import run
 from hal9.deploy import deploy
-from hal9.iobind import load, save
+from hal9.iobind import load, save, input
 from hal9.code import extract
---
+++ python/hal9/filetotext.py
@@ -0,0 +1,33 @@
+import os
+import tempfile
+import urllib.parse
+import urllib.request
+import textract
+import importlib.util
+
+def extract_from_url(message):
+  try:
+    result = urllib.parse.urlparse(message)
+    if not all([result.scheme, result.netloc]):
+      return message
+  except ValueError:
+    return message
+
+  textract_spec = importlib.util.find_spec("textract")
+  if textract_spec is None:
+    return message
+
+  try:
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+      with urllib.request.urlopen(message) as response:
+        temp_file.write(response.read())
+      temp_file_path = temp_file.name
+  except Exception as e:
+    return message
+
+  try:
+    text = textract.process(temp_file_path).decode('utf-8')
+    os.remove(temp_file_path)
+    return text
+  except Exception as e:
+    return message
