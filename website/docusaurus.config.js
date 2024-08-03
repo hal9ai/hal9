ea11e59f90f58a0d0f0e07f4e7170c92724c6cc5
Javier Luraschi <jluraschi@gmail.com>
2024-08-02 21:38:02 -0700

    use proper header title

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Hal9: Create AI Coworkers',
+  tagline: 'Hal9 provides enterprise-ready, secure, customizable, model-agnostic AI coworkers powered by the best AI libraries.',
   favicon: 'img/favicon.ico',
   url: 'https://hal9.com',
   baseUrl: '/docs/',
