92ffc58e0b153bdc917c17c8db6bf262f669ccfc
Javier Luraschi <jluraschi@gmail.com>
2024-09-05 15:15:20 -0700

    thumbnail for flux and echo apps

--- apps/flux/app.py
+++ apps/flux/app.py
@@ -4,19 +4,17 @@ import replicate
 
 from PIL import Image
 from io import BytesIO
-import shutil
+import hal9 as h9
 
 def flux_image(prompt, filename):
-    output = replicate.run("black-forest-labs/flux-dev", input={"prompt": prompt})
-    image_url = output[0]
+  output = replicate.run("black-forest-labs/flux-dev", input={"prompt": prompt})
+  image_url = output[0]
     
-    response = requests.get(image_url)
-    response.raise_for_status()
+  response = requests.get(image_url)
+  response.raise_for_status()
     
-    image = Image.open(BytesIO(response.content))
-    image.save(filename, format="JPEG")
-
-    shutil.copy(filename, f"storage/{filename}")
+  image = Image.open(BytesIO(response.content))
+  h9.save(filename, image)
 
 prompt = input()
 flux_image(prompt, "hal9-flux.jpg")
