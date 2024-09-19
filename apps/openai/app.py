9a53e4a9848b6a19b0ff931f55dbffa063186973
Jucaski <136751539+Jucaski@users.noreply.github.com>
2024-09-19 15:20:22 -0600

    Using a base url to connect to the proxy (#417)

--- apps/openai/app.py
+++ apps/openai/app.py
@@ -3,8 +3,13 @@ from openai import OpenAI
 import hal9 as h9
 
 messages = h9.load("messages", [])
+client = OpenAI(
+    base_url="https://api.devel.hal9.com/proxy/server=https://api.openai.com/v1/",
+    api_key = "hal9"
+)
 
-completion = OpenAI().chat.completions.create(
+
+completion = client.chat.completions.create(
   model = "o1-preview",
   messages = [
     {"role": "user", "content": input()},
