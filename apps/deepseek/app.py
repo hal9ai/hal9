9f08211676ec6f8f214f66b790e8b21bbc95411e
Javier Luraschi <jluraschi@gmail.com>
2025-01-25 19:25:41 -0800

    [apps/deepseek] use reasoning model

--- apps/deepseek/app.py
+++ apps/deepseek/app.py
@@ -2,10 +2,10 @@ from openai import OpenAI
 
 client = OpenAI(
   api_key="hal9",
-  base_url="https://api.hal9.com/proxy/server=https://api.deepseek.com")
+  base_url="http://localhost:5000/proxy/server=https://api.deepseek.com")
 
 stream = client.chat.completions.create(
-    model="deepseek-chat",
+    model="deepseek-reasoner",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
         {"role": "user", "content": input()},
@@ -14,5 +14,8 @@ stream = client.chat.completions.create(
 )
 
 for chunk in stream:
-  if len(chunk.choices) > 0 and chunk.choices[0].delta.content is not None: 
-    print(chunk.choices[0].delta.content, end="")
+  if len(chunk.choices) > 0:
+    if chunk.choices[0].delta.reasoning_content is not None and chunk.choices[0].delta.reasoning_content:
+      print(chunk.choices[0].delta.reasoning_content, end="")
+    elif chunk.choices[0].delta.content is not None:
+      print(chunk.choices[0].delta.content, end="")
