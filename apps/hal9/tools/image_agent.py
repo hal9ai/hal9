e4b10a8bbb2cd8e5ca56c3701043249604941322
Javier Luraschi <jluraschi@gmail.com>
2025-03-08 19:38:36 -0800

    [apps] proper proxy url

--- apps/hal9/tools/image_agent.py
+++ apps/hal9/tools/image_agent.py
@@ -9,7 +9,7 @@ import base64
 from mimetypes import guess_type
 import json
 
-replicate = Client(api_token="any", base_url="http://localhost:5000/proxy/server=https://api.replicate.com")
+replicate = Client(api_token="any", base_url="https://api.hal9.com/proxy/server=https://api.replicate.com")
 
 ########################### Functions ##########################
 
