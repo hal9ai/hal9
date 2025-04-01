20e1ab0e358a19cbd841923b8f71ce91b3cf56a3
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2025-04-01 14:39:11 +0200

    refactor to employ browseruse for info extraction only

--- apps/webscrape/app.py
+++ apps/webscrape/app.py
@@ -10,22 +10,27 @@ import shutil
 import json
 import pandas as pd
 
-# use OpenAI as LLM
-from langchain_openai import ChatOpenAI
-
-# browser-use imports
+# browser-use imports and setup
 from browser_use import Agent, Browser, BrowserConfig, SystemPrompt, ActionResult
 from browser_use.browser.context import BrowserContextConfig, BrowserContext
 from browser_use.agent.service import Agent
 from browser_use.controller.service import Controller
+from langchain_openai import ChatOpenAI
 
 response = subprocess.call(["playwright", "install"], stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
 if response != 0: sys.exit("Couldn't install playwright!")
 
+# openai client setup
+from openai import OpenAI
+client = OpenAI(base_url="https://api.hal9.com/proxy/server=https://api.openai.com/v1/", api_key = os.environ['HAL9_TOKEN'])
+
+# csv file location
 dir = '.user'
 if (os.path.exists(dir) == False):
     os.makedirs(dir)
+csv_file = "staff.csv"
 
+# custom configure browseruse
 class CustomPrompt(SystemPrompt):
     def important_rules(self) -> str:
         # Get existing rules from parent class
@@ -33,28 +38,25 @@ class CustomPrompt(SystemPrompt):
 
         # Add your custom rules
         new_rules = """
-11. ON SAVING TO CSV:
-- When you are tasked to append information to a csv file, make use of the function save_to_csv.
-- All csv files should appear in the '.user' directory.
-- All columns must contain a simple piece of information, not ever a composition (e.g., no nested data types, no JSON objects!)
-12. DEFAULT TASK:
-- Your default task is to extract information on company staff, and append that to a file called 'staff.csv'.
+11. DEFAULT TASK:
+- Your default task is to extract information on company staff.
 - ONLY use the company's website to obtain employee information,
   NOT any other web pages the company or its employees might appear on.
   E.g., do NOT!!! look for GitHub contributors to a company's organization instead!
-- The csv file should have a header with column names. When you initially create the csv to append to,
-  create it with the following column names:
+  We are interested in the following pieces of information:
     - company name
     - team/department
     - job title
     - full name
-    - link to GitHub repository
-    - link to LinkedIn profile
-- For each and any of these columns, only use information that is available on the company website.
-  If a piece is not, leave the column empty for that user.
-- The default task is to be completed when the user prompt contains one or more company names. 
-  For example, if the user enters 'Hal9, Posit' you should extract information about staff at Hal9 and Posit,
-  and append that to '.user/staff.csv'.
+    - link to GitHub repository (only if mentioned on the page)
+    - link to LinkedIn profile (only if mentioned on the page)
+    - For each and any of these items, only use information that is available directly on the company website. If a piece is not, set it to an empty string.
+12. RETURN FORMAT
+- The extracted information shall be passed back as a list of JSON objects. Here is an example:
+
+13. OVERALL WORKFLOW
+- The DEFAULT TASK is to be completed whenever the user prompt contains one or more company names. 
+  For example, if the user enters 'Hal9, Posit' you should extract information about staff at Hal9 and Posit.
 - If the user asks you explicitly for something else, just do what you are asked to do.
 """
 
@@ -63,11 +65,8 @@ class CustomPrompt(SystemPrompt):
 
 controller = Controller()
 
-@controller.action('On saving information to csv') 
-def save_to_csv(json: str, save_path: str = '.user/staff.csv'):
-    with open(save_path, 'a') as f: 
-        pd.read_json(json).to_csv(f)
-    return ActionResult(extracted_content = f'Staff information appended to {save_path}.')
+def append_csv(data):
+    # tbd create or append to file
 
 llm = ChatOpenAI(
     model = "gpt-4o",
@@ -79,29 +78,45 @@ browser = Browser(
     config = BrowserConfig(headless = True)
 )
 
-config = BrowserContextConfig(
-    save_recording_path = "./output-files/",
-)
-
-context = BrowserContext(browser = browser, config = config)
-
-async def run(agent):
+async def run(agent, browser):
     print("Working!")
     history = await agent.run()
+    await browser.close()
     return history
 
+# openai task
+openai_prompt = "tbd"  
+
 async def main():
+    # ask browseruse to extract staff information
     prompt = h9.input()
     agent = Agent(
-        browser_context = context,
+        browser = browser,
         controller = controller,
         system_prompt_class = CustomPrompt,
         task = prompt,
         llm = llm,
         save_conversation_path="logs/conversation.json" 
     )
-    history = await run(agent)
-    await browser.close()
+    result = run(agent, browser).final_result()
+
+    # ask openai to generate a csv file from this
+    csv_prompt = openai_prompt + "\n" + result
+
+    messages = h9.load("messages", [])
+    messages.append({"role": "user", "content": csv_prompt})
+    completion = client.chat.completions.create(model = "o1-preview", messages = messages, stream = True)
+    h9.save("messages", messages, hidden = True) # needed???
+    response = ""
+    for chunk in completion:
+        if chunk.choices and chunk.choices[0].delta and chunk.choices[0].delta.content:
+            content = chunk.choices[0].delta.content
+            print(content, end="")
+            response += content
+
+    # append to existing csv file
+    append_csv(response)
+    print(os.path.join("Staff information saved at: ", dir, csv_file))
 
 asyncio.run(main())
 
